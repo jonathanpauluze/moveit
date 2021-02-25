2cf78e8605ab7b5d8e744f45532e8be8486381d9
Jonathan Pauluze <jonathanpauluze@gmail.com>
2021-02-25 00:38:47 -0300

    create components using context

--- src/components/CompletedChallenges.tsx
+++ src/components/CompletedChallenges.tsx
@@ -1,10 +1,14 @@
+import { useChallenges } from '../contexts/ChallengesContext';
+
 import styles from '../styles/components/CompletedChallenges.module.css';
 
 export function CompletedChallenges() {
+  const { completedChallenges } = useChallenges();
+
   return (
     <div className={styles.completedChallengesContainer}>
       <span>Desafios completos</span>
-      <span>4</span>
+      <span>{completedChallenges}</span>
     </div>
   );
 }
--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,10 +1,19 @@
 import { useState, useEffect } from 'react';
 
+import { TimesIcon } from './TimesIcon';
+
 import styles from '../styles/components/Countdown.module.css';
+import { useChallenges } from '../contexts/ChallengesContext';
+
+const TIME_START = 25 * 60;
+let countdownTimeout: NodeJS.Timeout;
 
 export function Countdown() {
-  const [time, setTime] = useState(25 * 60);
-  const [active, setActive] = useState(false);
+  const { startNewChallenge } = useChallenges();
+
+  const [time, setTime] = useState(0.1 * 60);
+  const [isActive, setIsActive] = useState(false);
+  const [hasFinished, setHasFinished] = useState(false);
 
   const minutes = Math.floor(time / 60);
   const seconds = time % 60;
@@ -13,16 +22,27 @@ export function Countdown() {
   const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
 
   function startCountdown() {
-    setActive(true);
+    setIsActive(true);
+  }
+
+  function resetCountdown() {
+    clearTimeout(countdownTimeout);
+
+    setIsActive(false);
+    setTime(0.1 * 60);
   }
 
   useEffect(() => {
-    if (active && time > 0) {
-      setTimeout(() => {
+    if (isActive && time > 0) {
+      countdownTimeout = setTimeout(() => {
         setTime(time - 1);
       }, 1000);
+    } else if (isActive && time === 0) {
+      setHasFinished(true);
+      setIsActive(false);
+      startNewChallenge();
     }
-  }, [active, time]);
+  }, [isActive, time]);
 
   return (
     <>
@@ -38,13 +58,37 @@ export function Countdown() {
         </div>
       </div>
 
-      <button
-        type="button"
-        className={styles.countdownButton}
-        onClick={startCountdown}
-      >
-        Iniciar um ciclo
-      </button>
+      { hasFinished ? (
+        <button
+          disabled
+          className={styles.countdownButton}
+        >
+          Ciclo encerrado
+          <img src="icons/check.svg" alt="Concluído" />
+        </button>
+      ) : (
+        <>
+          { isActive ? (
+            <button
+              type="button"
+              className={`${styles.countdownButton} ${styles.countdownButtonActive}`}
+              onClick={resetCountdown}
+            >
+              Abandonar ciclo
+              <TimesIcon />
+            </button>
+          ) : (
+            <button
+              type="button"
+              className={styles.countdownButton}
+              onClick={startCountdown}
+            >
+              Iniciar um ciclo
+              <img src="icons/play.svg" alt="Iniciar" />
+            </button>
+          ) }
+        </>
+      )}
     </>
   );
 }
