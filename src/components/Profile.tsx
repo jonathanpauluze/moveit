30615761b443766ec7bd88107ccbaa1ccf8eb54c
Jonathan Pauluze <jonathanpauluze@gmail.com>
2021-02-26 00:57:10 -0300

    fix: remove static level information

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useChallenges } from '../contexts/ChallengesContext';
+
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+  const { level } = useChallenges();
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/jonathanpauluze.png" alt="Jonathan Pauluze" />
@@ -8,7 +12,7 @@ export function Profile() {
         <strong>Jonathan Pauluze</strong>
         <p>
           <img src="icons/level.svg" alt="Level" />
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
