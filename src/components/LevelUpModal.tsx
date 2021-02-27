6116cbd197827d6592b02a9541c09b2a0c511d06
Jonathan Pauluze <jonathanpauluze@gmail.com>
2021-02-27 11:29:31 -0300

    feat: create level up modal

---
+++ src/components/LevelUpModal.tsx
@@ -0,0 +1,22 @@
+import { useChallenges } from '../contexts/ChallengesContext';
+
+import styles from '../styles/components/LevelUpModal.module.css';
+
+export function LevelUpModal() {
+  const { level, closeLevelUpModal } = useChallenges();
+
+  return (
+    <div className={styles.overlay}>
+      <div className={styles.container}>
+        <header>{level}</header>
+
+        <strong>Parabéns!</strong>
+        <p>Você alcançou um novo level</p>
+
+        <button type="button" onClick={closeLevelUpModal}>
+          <img src="/icons/close.svg" alt="Fechar"/>
+        </button>
+      </div>
+    </div>
+  );
+}
