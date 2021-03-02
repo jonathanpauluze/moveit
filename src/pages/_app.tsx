03bc240dbc26f51e28c75627fcc63e6613fb192b
Jonathan Pauluze <jonathanpauluze@gmail.com>
2021-03-01 21:17:26 -0300

    feat: create toggle theme function

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,27 @@
-import '../styles/global.css';
+import { FC, useState } from 'react';
+import { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+import GlobalStyles from '../styles/global';
+
+import light from '../styles/themes/light';
+import dark from '../styles/themes/dark';
+
+const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
+  const [theme, setTheme] = useState(light);
+
+  const toggleTheme = () => {
+    setTheme(theme.title === 'light' ? dark : light);
+  }
+
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} toggleTheme={toggleTheme} />
+        <GlobalStyles />
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
