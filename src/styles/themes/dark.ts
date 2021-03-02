ed1aeaaecd9f1c534503b62a156a0ca67f363bb4
Jonathan Pauluze <jonathanpauluze@gmail.com>
2021-03-01 21:15:23 -0300

    create light and dark themes

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,20 @@
+const dark = {
+  title: 'dark',
+
+  colors: {
+    white: '#fff',
+    main: '#373737',
+    background: '#262626',
+    grayLine: '#DCDDE0',
+    text: '#fff',
+    textHighlight: '#B3B9FF',
+    title: '#336ade',
+    red: '#E83F5B',
+    green: '#4CD62B',
+    blue: '#5965E0',
+    blueDark: '#4953B8',
+    blueTwitter: '#2AA9E0'
+  }
+}
+
+export default dark;
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,20 @@
+const light = {
+  title: 'light',
+
+  colors: {
+    white: '#fff',
+    main: '#FFF',
+    background: '#F2F3F5',
+    grayLine: '#DCDDE0',
+    text: '#666',
+    textHighlight: '#B3B9FF',
+    title: '#2E384D',
+    red: '#E83F5B',
+    green: '#4CD62B',
+    blue: '#5965E0',
+    blueDark: '#4953B8',
+    blueTwitter: '#2AA9E0'
+  }
+}
+
+export default light;
